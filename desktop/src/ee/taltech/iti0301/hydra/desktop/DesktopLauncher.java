d457e76d504c6e9023730f4b199ed1b13088ad66
Pavel Grubeljas <pagrub@ttu.ee>
2022-02-10 20:00:15 +0200

    First kinda commit #2

---
+++ desktop/src/ee/taltech/iti0301/hydra/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ee.taltech.iti0301.hydra.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ee.taltech.iti0301.hydra.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
