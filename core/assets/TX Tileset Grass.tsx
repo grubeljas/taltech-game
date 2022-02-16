0d14bb600378f82d0f445bae46d8ce53d60d8f07
Anton <anton.grigorenko@ttu.ee>
2022-02-16 14:13:47 +0200

    Added a map and tiles in assets. Right now not working.

---
+++ core/assets/TX Tileset Grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="TX Tileset Grass" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="49" columns="7">
+ <image source="OneDrive/Töölaud/Texture/TX Tileset Grass.png" width="256" height="256"/>
+</tileset>
