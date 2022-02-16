bb1cba24ec90b814d3c1374abcefe12354734058
Anton <anton.grigorenko@ttu.ee>
2022-02-16 21:30:39 +0200

    - Added a map and tiles in assets. - Map showing up on screen.

--- core/src/ee/taltech/iti0301/hydra/screens/GameScreen.java
+++ core/src/ee/taltech/iti0301/hydra/screens/GameScreen.java
@@ -4,9 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import ee.taltech.iti0301.hydra.Hydra;
@@ -16,6 +16,8 @@ public class GameScreen implements Screen {
     public static final float SPEED = 140;
     Texture img;
     TiledMap tiledMap;
+    OrthogonalTiledMapRenderer renderer;
+    OrthographicCamera camera;
 
     float x;
     float y;
@@ -24,17 +26,19 @@ public class GameScreen implements Screen {
 
     public GameScreen(Hydra hydra) {
         this.hydra = hydra;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 50, 50 * (8f / 12f));
     }
 
     @Override
     public void show () {
         img = new Texture("badlogic.jpg");
-        tiledMap = new TmxMapLoader().load("FirstMap.tmx");
+        tiledMap = new TmxMapLoader().load("Map_assets/FirstMap.tmx");
+        renderer = new OrthogonalTiledMapRenderer(tiledMap, 1/32f);
     }
 
     @Override
     public void render (float delta) {
-        hydra.batch.begin();
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             x += SPEED * Gdx.graphics.getDeltaTime();
         }
@@ -49,6 +53,11 @@ public class GameScreen implements Screen {
         }
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+
+        hydra.batch.begin();
         hydra.batch.draw(img, x, y);
         hydra.batch.end();
     }
