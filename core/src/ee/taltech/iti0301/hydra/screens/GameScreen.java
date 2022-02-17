b2cd53af6b729d37967067ec3771e454bf223ba0
Mikhail Kozhevnikov <mikozh@ttu.ee>
2022-02-17 05:12:48 +0200

    Issue #6: Camera is now tied to the tank texture and follows it, with the tank remaining in the middle. Also, some refactoring (inputs are now in their own method - handleInput).

--- core/src/ee/taltech/iti0301/hydra/screens/GameScreen.java
+++ core/src/ee/taltech/iti0301/hydra/screens/GameScreen.java
@@ -13,10 +13,10 @@ import ee.taltech.iti0301.hydra.Hydra;
 
 public class GameScreen implements Screen {
 
-    public static final float SPEED = 15;
-    Texture img;
+    public static final float SPEED = 20;
+    Texture tempTankTexture;
     TiledMap tiledMap;
-    OrthogonalTiledMapRenderer renderer;
+    OrthogonalTiledMapRenderer mapRenderer;
     OrthographicCamera camera;
 
     float x;
@@ -28,38 +28,47 @@ public class GameScreen implements Screen {
         this.hydra = hydra;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 50, 50 * (8f / 12f));
+
+        tempTankTexture = new Texture("prototank.png");
+        tiledMap = new TmxMapLoader().load("Map_assets/FirstMap.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1/32f);
+    }
+
+    private void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            x += SPEED * Gdx.graphics.getDeltaTime();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            y -= SPEED * Gdx.graphics.getDeltaTime();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            x -= SPEED * Gdx.graphics.getDeltaTime();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            y += SPEED * Gdx.graphics.getDeltaTime();
+        }
     }
 
     @Override
     public void show () {
-        img = new Texture("prototank.png");
-        tiledMap = new TmxMapLoader().load("Map_assets/FirstMap.tmx");
-        renderer = new OrthogonalTiledMapRenderer(tiledMap, 1/32f);
     }
 
     @Override
     public void render (float delta) {
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            x += SPEED * Gdx.graphics.getDeltaTime();
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            y -= SPEED * Gdx.graphics.getDeltaTime();
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            x -= SPEED * Gdx.graphics.getDeltaTime();
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            y += SPEED * Gdx.graphics.getDeltaTime();
-        }
+
+        handleInput();
+        camera.position.x = x;
+        camera.position.y = y;
+        camera.update();
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        camera.update();
-        renderer.setView(camera);
-        renderer.render();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
 
         hydra.batch.setProjectionMatrix(camera.combined);
         hydra.batch.begin();
-        hydra.batch.draw(img, x, y, 3, 3);
+        hydra.batch.draw(tempTankTexture, x - 1, y - 1, 3, 3);
         hydra.batch.end();
     }
 
