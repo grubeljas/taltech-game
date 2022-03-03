e07243e9232e78048d5fb4896d9928f8f1f70187
Anton <anton.grigorenko@ttu.ee>
2022-03-03 09:15:42 +0200

    Added possibility to zoom in and out the map. Zoom in by holding Q, zoom out by holding E.

---
+++ core/assets/Map_assets/decor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="decor" tilewidth="32" tileheight="32" tilecount="512" columns="16">
+ <image source="decor.png" width="512" height="1024"/>
+</tileset>
